79681520ce2da4aa60f9b649c00096199112ce39
Christophe Guillonnet <christophe.guillonnet@eisti.fr>
2014-06-17 21:05:41 +0200

    debut

---
+++ src/modele/Salle.java
@@ -0,0 +1,18 @@
+package modele ;
+
+import modele.*;
+import java.util.ArrayList ;
+
+public class Salle {
+	String nom ;
+	ArrayList<Table> tables ;
+
+	public String getNom(){
+		return this.nom;
+	}
+
+	public ArrayList<Tables> getTables(){
+		return this.tables ;
+	}
+
+}
